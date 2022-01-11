83de23c739f55575b22ae15ade6e115fecba4e48
gonzalolater <gonzalolater>
2022-01-11 16:14:30 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
   return (
     <div className="App">
       <Router>
+          {/* MENULOGIN */}
         <Navbar />
         <Switch>
           <Route exact path="/" component={Home} />
